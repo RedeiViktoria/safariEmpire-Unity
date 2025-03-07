e4e18def1119c6a2a1a008dcfe3141d073be7707
Rédei Viktória <bqjtv6@inf.elte.hu>
2025-03-07 15:54:53 +0100

    teszt2

--- SafariEmpire/Assets/Codes/Model.cs
+++ SafariEmpire/Assets/Codes/Model.cs
@@ -7,10 +7,10 @@ public class Model : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        /*
+        
         GameObject obj = GameObject.Find("Square");
         SpriteRenderer sr = obj.GetComponent<SpriteRenderer>();
-        sr.color = Color.blue;*/
+        sr.color = Color.blue;
     }
 
     // Update is called once per frame
