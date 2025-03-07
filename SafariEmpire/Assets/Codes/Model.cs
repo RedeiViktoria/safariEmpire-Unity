550d24b67f85b220ffddb01ac2a7e6dbfb609ac9
Rédei Viktória <bqjtv6@inf.elte.hu>
2025-03-07 15:33:15 +0100

    codes mappa

---
+++ SafariEmpire/Assets/Codes/Model.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Model : MonoBehaviour
+{
+    private int difficulty;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        /*
+        GameObject obj = GameObject.Find("Square");
+        SpriteRenderer sr = obj.GetComponent<SpriteRenderer>();
+        sr.color = Color.blue;*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
