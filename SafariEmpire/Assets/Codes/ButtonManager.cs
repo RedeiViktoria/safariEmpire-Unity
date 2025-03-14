b3de8f895d7ac2f19ab12f61cb147eaf3657e613
Keller Bálint Krisztián <ya2xen@inf.elte.hu>
2025-03-14 10:33:10 +0100

    UI update
    Kamera mozgatás implementálva, bolt és safari menüben lévő gombok bekötve. Bolt és safari menü becsukható most már ha ki kattintunk belőle.

--- SafariEmpire/Assets/Codes/ButtonManager.cs
+++ SafariEmpire/Assets/Codes/ButtonManager.cs
@@ -8,6 +8,7 @@ public class ButtonManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Button shopBtn;
     public Button safariBtn;
+    public GameObject layout;
     public GameObject timeButtons;
     public GameObject shop;
     public GameObject safari;
@@ -16,9 +17,11 @@ public class ButtonManager : MonoBehaviour
     public GameObject security;
     void Start()
     {
+        //Adding listeners to open the shop and the safari menu, and close if something else.
         shopBtn.onClick.AddListener(OnShopClicked);
         safariBtn.onClick.AddListener(OnSafariClicked);
-        
+        layout.GetComponent<Button>().onClick.AddListener(OnLayoutClicked);
+        //Adding listeners to the time buttons in the mainUI
         foreach (Transform child in timeButtons.transform)
         {
             Button button = child.GetComponent<Button>();
@@ -41,6 +44,7 @@ public class ButtonManager : MonoBehaviour
             }
         }
 
+        //Adding listeners to switch buttons in safari menu
         Transform switchButtons = safari.transform.Find("Buttons");
         int i = 0;
         foreach (Transform child in switchButtons.transform)
@@ -49,6 +53,57 @@ public class ButtonManager : MonoBehaviour
             Button button = child.GetComponent<Button>();
             button.onClick.AddListener(() => OnSwitchClicked(btnIndex));
             i += 1;
+        }
+
+        //Adding listeners to shop buttons
+        foreach (Transform parent in shop.transform)
+        {
+            foreach (Transform child in parent)
+            {
+                Button button = child.GetComponent<Button>();
+
+                if (button != null)
+                {
+                    Button capturedButton = button;
+                    capturedButton.onClick.AddListener(() => OnShopButtonClicked(parent.name));
+                }
+            }
+        }
+
+        //Adding listener to ticket price setter button
+        foreach(Transform child in menu.transform)
+        {
+            Button priceButton = child.GetComponent<Button>();
+            if (priceButton != null)
+            {
+                priceButton.onClick.AddListener(() => Debug.Log("Price set"));
+            }
+        }
+
+        //Adding listeners to poacher target buttons
+        foreach(Transform parent in vadorok.transform)
+        {
+            foreach (Transform child in parent)
+            {
+                Button button = child.GetComponent<Button>();
+                if (button != null)
+                {
+                    button.onClick.AddListener(() => OnPoacherTargetClicked());
+                }
+            }
+        }
+
+        //Adding listeners to security path buttons
+        foreach (Transform parent in security.transform)
+        {
+            foreach (Transform child in parent)
+            {
+                Button button = child.GetComponent<Button>();
+                if (button != null)
+                {
+                    button.onClick.AddListener(() => OnSecurityPathClicked());
+                }
+            }
         }
     }
 
@@ -66,6 +121,11 @@ public class ButtonManager : MonoBehaviour
     void OnSafariClicked()
     {
         safari.SetActive(!safari.activeSelf);
+        shop.SetActive(false);
+    }
+    void OnLayoutClicked()
+    {
+        safari.SetActive(false);
         shop.SetActive(false);
     }
     void OnTimeClicked(int timeSpeed)
@@ -107,8 +167,18 @@ public class ButtonManager : MonoBehaviour
             security.SetActive(true);
         }
     }
-    void OnBuyClicked()
+    void OnShopButtonClicked(string name)
     {
-        Debug.Log("clicked2");
+        Debug.Log(name);
+    }
+
+    void OnPoacherTargetClicked()
+    {
+        Debug.Log("Poacher clicked");
+    }
+
+    void OnSecurityPathClicked()
+    {
+        Debug.Log("Security path clicked");
     }
 }
